7b45fea5d7246adf0150d510dda0edb5339c029a
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-01-23 19:28:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1921,7 +1921,7 @@ async def auto_filter(client, msg, spoll=False):
     # else:
     if settings["button"]:
         cap+="<b>\n\n<u>📚 Requested Files 👇</u></b>\n"
-            for file in files:
+        for file in files:
                 cap += f"<b>\n💠 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     # if imdb and imdb.get('poster'):
     #     try:
