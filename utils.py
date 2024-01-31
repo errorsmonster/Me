dcfeba1afb360d2d4f37f7c8b40280037093a6d8
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-02-01 02:35:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1042,11 +1042,4 @@ def get_hash(media_msg: Message) -> str:
     return getattr(media, "file_unique_id", "")[:6]
 
 
-async def get_cap(settings, remaining_seconds, files, query, total_results, search):
-    
-        cap = f"<b>Hᴇʏ {query.from_user.mention}, Fᴏᴜɴᴅ {total_results} Rᴇsᴜʟᴛs ғᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ {search}\n\n</b>"
-        for file in files:
-            cap += f"<b>♻️ <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
-        return cap
-
 
