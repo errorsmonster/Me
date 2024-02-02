8fdce64a09d2802debd758bc5d141c9c63d30272
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-02-03 00:03:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -421,7 +421,7 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
         time_difference = timedelta(hours=cur_time.hour, minutes=cur_time.minute, seconds=(cur_time.second+(cur_time.microsecond/1000000))) - timedelta(hours=curr_time.hour, minutes=curr_time.minute, seconds=(curr_time.second+(curr_time.microsecond/1000000)))
         remaining_seconds = "{:.2f}".format(time_difference.total_seconds())
         total_results = len(files)
-        cap = await (settings, remaining_seconds, files, query, total_results, search)
+        cap = await get_cap(settings, remaining_seconds, files, query, total_results, search)
         try:
             await query.message.edit_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
         except MessageNotModified:
